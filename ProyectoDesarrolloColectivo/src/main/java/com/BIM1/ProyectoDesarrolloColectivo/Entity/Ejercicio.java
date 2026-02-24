a75717d5bde6dba708943dfa4e738debaec9fb5d
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 17:57:30 -0600

    feat: se ha agregado los getter y setter de Ejercicio

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Ejercicio.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Ejercicio.java
@@ -1,5 +1,11 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Ejercicio")
 public class Ejercicio {
 
     @Id
@@ -31,5 +37,59 @@ public class Ejercicio {
     @NotNull
     private Integer fk_id_rutina;
 
+    public Integer getId_ejercicio() {
+        return id_ejercicio;
+    }
 
+    public void setId_ejercicio(Integer id_ejercicio) {
+        this.id_ejercicio = id_ejercicio;
+    }
+
+    public String getNombre_ejercicio() {
+        return nombre_ejercicio;
+    }
+
+    public void setNombre_ejercicio(String nombre_ejercicio) {
+        this.nombre_ejercicio = nombre_ejercicio;
+    }
+
+    public int getSeries_ejercicio() {
+        return series_ejercicio;
+    }
+
+    public void setSeries_ejercicio(int series_ejercicio) {
+        this.series_ejercicio = series_ejercicio;
+    }
+
+    public int getRepeticiones_ejercicio() {
+        return repeticiones_ejercicio;
+    }
+
+    public void setRepeticiones_ejercicio(int repeticiones_ejercicio) {
+        this.repeticiones_ejercicio = repeticiones_ejercicio;
+    }
+
+    public int getTiempo_ejercicio() {
+        return tiempo_ejercicio;
+    }
+
+    public void setTiempo_ejercicio(int tiempo_ejercicio) {
+        this.tiempo_ejercicio = tiempo_ejercicio;
+    }
+
+    public int getDescanso_ejercicio() {
+        return descanso_ejercicio;
+    }
+
+    public void setDescanso_ejercicio(int descanso_ejercicio) {
+        this.descanso_ejercicio = descanso_ejercicio;
+    }
+
+    public Integer getFk_id_rutina() {
+        return fk_id_rutina;
+    }
+
+    public void setFk_id_rutina(Integer fk_id_rutina) {
+        this.fk_id_rutina = fk_id_rutina;
+    }
 }
