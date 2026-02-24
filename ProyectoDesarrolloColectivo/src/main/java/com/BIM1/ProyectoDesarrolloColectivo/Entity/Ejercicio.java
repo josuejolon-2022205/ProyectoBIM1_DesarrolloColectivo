e84ecf5c10cd5f4e845bd83cd82c06ecfe3e8b33
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 17:50:48 -0600

    feat: se han agregado los atributos de Ejercicio

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Ejercicio.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Ejercicio.java
@@ -1,4 +1,35 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
 public class Ejercicio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ejercicio")
+    private Integer id_ejercicio;
+
+    @Column(name = "nombre_ejercicio")
+    @NotBlank
+    private String nombre_ejercicio;
+
+    @Column(name = "series_ejercicio")
+    @NotNull
+    private int series_ejercicio;
+
+    @Column(name = "repeticiones_ejercicio")
+    @NotNull
+    private int repeticiones_ejercicio;
+
+    @Column(name = "tiempo_ejercicio")
+    @NotNull
+    private int tiempo_ejercicio;
+
+    @Column(name = "descanso_ejercicio")
+    @NotNull
+    private int descanso_ejercicio;
+
+    @Column(name = "fk_id_rutina")
+    @NotNull
+    private Integer fk_id_rutina;
+
+
 }
