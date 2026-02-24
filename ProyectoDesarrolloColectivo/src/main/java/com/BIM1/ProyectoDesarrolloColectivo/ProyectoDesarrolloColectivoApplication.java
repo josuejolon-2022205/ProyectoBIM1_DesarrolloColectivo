9cad38436d26478b06e89cbe3dee01d3e73b54a0
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 16:19:19 -0600

    feat:Se ha agregado la carpeta del proyecto para backend

---
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/ProyectoDesarrolloColectivoApplication.java
@@ -0,0 +1,13 @@
+package com.BIM1.ProyectoDesarrolloColectivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDesarrolloColectivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDesarrolloColectivoApplication.class, args);
+	}
+
+}
