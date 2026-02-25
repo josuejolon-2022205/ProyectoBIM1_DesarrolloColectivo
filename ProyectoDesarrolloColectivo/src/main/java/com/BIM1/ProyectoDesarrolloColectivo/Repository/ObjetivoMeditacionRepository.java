e6e24c2c0a0bf1a96babfcc27b243e207ec4ea85
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-02-24 21:25:21 -0600

    feat: Se ha agregado el JpaRepository en ObjetivoMeditacionRepository

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/ObjetivoMeditacionRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/ObjetivoMeditacionRepository.java
@@ -1,4 +1,9 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Repository;
 
-public interface ObjetivoMeditacionRepository {
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.ObjetivoMeditacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ObjetivoMeditacionRepository extends JpaRepository<ObjetivoMeditacion, Integer> {
 }
