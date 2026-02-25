2968f3302bf879eebd15bb9ad2ae24a4a11df162
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 18:41:50 -0600

    feat: se ha agregado los getter y setter de Rutina

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Rutina.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Rutina.java
@@ -1,5 +1,10 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "Rutina")
 public class Rutina {
 
     @Id
@@ -19,6 +24,35 @@ public class Rutina {
     @NotBlank
     private Integer fk_id_usuario;
 
-    
+    public Integer getId_rutina() {
+        return id_rutina;
+    }
 
+    public void setId_rutina(Integer id_rutina) {
+        this.id_rutina = id_rutina;
+    }
+
+    public String getNombre_rutina() {
+        return nombre_rutina;
+    }
+
+    public void setNombre_rutina(String nombre_rutina) {
+        this.nombre_rutina = nombre_rutina;
+    }
+
+    public String getDias_semana() {
+        return dias_semana;
+    }
+
+    public void setDias_semana(String dias_semana) {
+        this.dias_semana = dias_semana;
+    }
+
+    public Integer getFk_id_usuario() {
+        return fk_id_usuario;
+    }
+
+    public void setFk_id_usuario(Integer fk_id_usuario) {
+        this.fk_id_usuario = fk_id_usuario;
+    }
 }
