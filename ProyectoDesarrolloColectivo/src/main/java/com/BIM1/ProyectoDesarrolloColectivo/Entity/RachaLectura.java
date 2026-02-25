9d008e684fc01cedc8faf555128ee6fc5d5723d3
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-02-24 20:02:33 -0600

    feat: se completo el Entity RachaLectura con campos y anotaciones

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/RachaLectura.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/RachaLectura.java
@@ -1,4 +1,55 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Racha_lectura")
 public class RachaLectura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_racha_lectura")
+    private Integer idRachaLectura;
+
+    @Column(name = "dias_consecutivos")
+    private Integer diasConsecutivos;
+
+    @Column(name = "fecha")
+    private LocalDate fecha;
+
+    @Column(name = "fk_id_usuario")
+    private Integer fkIdUsuario;
+
+    public Integer getIdRachaLectura() {
+        return idRachaLectura;
+    }
+
+    public void setIdRachaLectura(Integer idRachaLectura) {
+        this.idRachaLectura = idRachaLectura;
+    }
+
+    public Integer getDiasConsecutivos() {
+        return diasConsecutivos;
+    }
+
+    public void setDiasConsecutivos(Integer diasConsecutivos) {
+        this.diasConsecutivos = diasConsecutivos;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getFkIdUsuario() {
+        return fkIdUsuario;
+    }
+
+    public void setFkIdUsuario(Integer fkIdUsuario) {
+        this.fkIdUsuario = fkIdUsuario;
+    }
 }
