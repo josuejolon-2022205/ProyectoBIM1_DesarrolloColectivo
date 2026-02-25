d0f38783ceace25d2e1415f9cfa7a68c7e636dc4
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-02-24 19:58:14 -0600

    feat: Se ha agregado la clase UsuarioRepository con el JpaRepository en Repository

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/UsuarioRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/UsuarioRepository.java
@@ -1,4 +1,16 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Repository;
 
-public interface UsuarioRepository {
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    boolean existsByNombre_CompletoAndCorreo_UsuarioAndContraseña(
+            String nombre_completo,
+            String correo_usuario,
+            String contraseña
+    );
 }
+
