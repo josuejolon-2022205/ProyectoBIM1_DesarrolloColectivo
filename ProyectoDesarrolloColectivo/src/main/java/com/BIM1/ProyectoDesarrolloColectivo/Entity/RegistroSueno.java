9c07ec3ecbc57be317f473b02e14705fdb5c1181
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-02-24 22:34:23 -0600

    feat: Se han agregado los métodos getters and setters en RegistroSueño de Entity

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/RegistroSueno.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/RegistroSueno.java
@@ -1,4 +1,74 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
-public class RegistroSueño {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "RegistrosSueños")
+public class RegistroSueno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_registro_sueño")
+    private Integer id_registro_sueño;
+
+    @NotBlank(message = "La fecha no puede estar vacío")
+    @Column(name = "fecha_sueño")
+    private String fecha_sueño;
+
+    @NotBlank(message = "Las horas dormidas no pueden estar vacías")
+    @Column(name = "horas_dormidas")
+    private String horas_dormidas;
+
+    @NotBlank(message = "La calidad del sueño no puede estar vacío")
+    @Column(name = "calidad_sueño")
+    private String calidad_sueño;
+
+    @NotBlank(message = "El fk_id_usuario no puede estar vacío")
+    @Column(name = "fk_id_usuario")
+    private Integer fk_id_usuario;
+
+    public RegistroSueno() {
+
+    }
+
+    public Integer getId_registro_sueño() {
+        return id_registro_sueño;
+    }
+
+    public void setId_registro_sueño(Integer id_registro_sueño) {
+        this.id_registro_sueño = id_registro_sueño;
+    }
+
+    public String getFecha_sueño() {
+        return fecha_sueño;
+    }
+
+    public void setFecha_sueño(String fecha_sueño) {
+        this.fecha_sueño = fecha_sueño;
+    }
+
+    public String getHoras_dormidas() {
+        return horas_dormidas;
+    }
+
+    public void setHoras_dormidas(String horas_dormidas) {
+        this.horas_dormidas = horas_dormidas;
+    }
+
+    public String getCalidad_sueño() {
+        return calidad_sueño;
+    }
+
+    public void setCalidad_sueño(String calidad_sueño) {
+        this.calidad_sueño = calidad_sueño;
+    }
+
+    public Integer getFk_id_usuario() {
+        return fk_id_usuario;
+    }
+
+    public void setFk_id_usuario(Integer fk_id_usuario) {
+        this.fk_id_usuario = fk_id_usuario;
+    }
 }
