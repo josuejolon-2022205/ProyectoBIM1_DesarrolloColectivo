bd53e331e1789a9a10345a71031c1fc2c2d0edf6
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 19:22:53 -0600

    feat: se ha implmentado el crud en el serviceImplements para Libro

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/LibroServiceImplements.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/LibroServiceImplements.java
@@ -1,4 +1,63 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Service;
 
-public class LibroServiceImplements {
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.Libro;
+import com.BIM1.ProyectoDesarrolloColectivo.Exceptions.Exception;
+import com.BIM1.ProyectoDesarrolloColectivo.Repository.LibroRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LibroServiceImplements  implements LibroService {
+    private final LibroRepository libroRepository;
+
+    public LibroServiceImplements(LibroRepository libroRepository) {
+        this.libroRepository = libroRepository;
+    }
+
+    @Override
+    public List<Libro> getAListLibro() {
+        return libroRepository.findAll();
+    }
+
+    @Override
+    public Libro getLibroById(Integer id) {
+        Libro libro = libroRepository.findById(id).orElse(null);
+        if(libro == null){
+            throw new IllegalArgumentException();
+        }else {
+            return libroRepository.findById(id).orElse(null);
+        }
+    }
+
+    @Override
+    public Libro saveLibro(Libro libro) throws RuntimeException {
+        return libroRepository.save(libro);
+    }
+
+    @Override
+    public Libro updateLibro(Integer id, Libro libro) {
+
+        Libro libros = libroRepository.findById(id).orElse(null);
+        if(libros != null){
+            libros.setTitulo_libro(libro.getTitulo_libro());
+            libros.setAutor_libro(libro.getAutor_libro());
+            libros.setEstado(libro.getEstado());
+            libros.setCantidad_pag(libro.getCantidad_pag());
+            libros.setCantidad_leido(libro.getCantidad_leido());
+
+        }else{
+            throw new Exception("el id del libro no existe");
+        }
+        return libroRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteLibro(Integer id) {
+        Libro libros = libroRepository.findById(id).orElse(null);
+        if(libros != null){
+            throw new Exception("el id del libro no existe");
+        }
+        libroRepository.deleteById(id);
+    }
 }
