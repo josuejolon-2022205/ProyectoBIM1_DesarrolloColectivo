2c6917d6abefdbbaa9efa05bfbd5241eab62cc22
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 19:08:47 -0600

    feat: se ha implementado el crud en el serviceImplements

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/EjercicioServiceImplements.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/EjercicioServiceImplements.java
@@ -1,4 +1,64 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Service;
 
-public class EjercicioServiceImplements {
+
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.Ejercicio;
+import com.BIM1.ProyectoDesarrolloColectivo.Exceptions.Exception;
+import com.BIM1.ProyectoDesarrolloColectivo.Repository.EjercicioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EjercicioServiceImplements implements EjercicioService{
+    private final EjercicioRepository ejercicioRepository;
+
+    public EjercicioServiceImplements(EjercicioRepository ejercicioRepository) {
+        this.ejercicioRepository = ejercicioRepository;
+    }
+
+    @Override
+    public List<Ejercicio> getAListEjercicio() {
+        return ejercicioRepository.findAll();
+    }
+
+    @Override
+    public Ejercicio getEjercicioById(Integer id) {
+        Ejercicio ejercicio = ejercicioRepository.findById(id).orElse(null);
+        if(ejercicio == null){
+            throw new IllegalArgumentException();
+        }else {
+            return ejercicioRepository.findById(id).orElse(null);
+        }
+    }
+
+    @Override
+    public Ejercicio saveEjercicio(Ejercicio ejercicio) throws RuntimeException {
+        return ejercicioRepository.save(ejercicio);
+    }
+
+    @Override
+    public Ejercicio updateEjercicio(Integer id, Ejercicio ejercicio) {
+        Ejercicio ejercicios = ejercicioRepository.findById(id).orElse(null);
+        if(ejercicios != null){
+            ejercicios.setNombre_ejercicio(ejercicio.getNombre_ejercicio());
+            ejercicios.setSeries_ejercicio(ejercicio.getSeries_ejercicio());
+            ejercicios.setRepeticiones_ejercicio(ejercicio.getRepeticiones_ejercicio());
+            ejercicios.setTiempo_ejercicio(ejercicio.getTiempo_ejercicio());
+            ejercicios.setDescanso_ejercicio(ejercicio.getDescanso_ejercicio());
+            ejercicios.setFk_id_rutina(ejercicio.getFk_id_rutina());
+        }else{
+            throw new Exception("el id del ejercicio no existe");
+        }
+        return ejercicioRepository.save(ejercicios);
+    }
+
+    @Override
+    public void deleteEjercicio(Integer id) {
+        Ejercicio ejercicios = ejercicioRepository.findById(id).orElse(null);
+        if(ejercicios != null){
+            throw new Exception("el id del ejercicio no existe");
+        }
+        ejercicioRepository.deleteById(id);
+
+    }
 }
