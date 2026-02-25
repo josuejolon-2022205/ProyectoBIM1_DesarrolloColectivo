04e919cb0ff5c8d19fc59b96f028e83e5eea0911
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 18:23:51 -0600

    fet: se ha agregado los atributos de Libro

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Libro.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Libro.java
@@ -1,4 +1,35 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
 public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_libro")
+    private Integer id_libro;
+
+    @Column(name = "titulo_libro")
+    @NotBlank
+    private String titulo_libro;
+
+    @Column(name = "autor_libro")
+    @NotBlank
+    private String autor_libro;
+
+    @Column(name = "estado")
+    @NotBlank
+    private String estado;
+
+    @Column(name = "cantidad_pag")
+    @NotNull
+    private int cantidad_pag;
+
+    @Column(name = "cantidad_leido")
+    @NotNull
+    private int cantidad_leido;
+
+    @Column(name = "fk_id_usuario")
+    @NotNull
+    private int fk_id_usuario;
+
+
 }
