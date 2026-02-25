5c68f0daf7feac4ebb28644df3f2af7d7ef6cfc0
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 18:41:34 -0600

    feat: se ha agregado los atributos de Rutina

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Rutina.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Rutina.java
@@ -1,4 +1,24 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
 public class Rutina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rutina")
+    private Integer id_rutina;
+
+    @Column(name = "nombre_rutina")
+    @NotBlank
+    private String nombre_rutina;
+
+    @Column(name = "dias_semana")
+    @NotBlank
+    private String dias_semana;
+
+    @Column(name = "fk_id_usuario")
+    @NotBlank
+    private Integer fk_id_usuario;
+
+    
+
 }
