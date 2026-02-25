162785926318e0a5c1466535bb6bef316d362fee
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-02-24 23:37:48 -0600

    feat: Se han agregado los métodos al ObjetivoMeditacionServiceImplements del Service

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ObjetivoMeditacionServiceImplements.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ObjetivoMeditacionServiceImplements.java
@@ -1,4 +1,51 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Service;
 
-public class ObjetivoMeditacionServiceImplements {
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.ObjetivoMeditacion;
+import com.BIM1.ProyectoDesarrolloColectivo.Repository.ObjetivoMeditacionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ObjetivoMeditacionServiceImplements implements ObjetivoMeditacionService{
+
+    private final ObjetivoMeditacionRepository objetivoMeditacionRepository;
+
+    public ObjetivoMeditacionServiceImplements (ObjetivoMeditacionRepository objetivoMeditacionRepository) {
+        this.objetivoMeditacionRepository = objetivoMeditacionRepository;
+    }
+
+    @Override
+    public List<ObjetivoMeditacion> getAllObjetivosMeditacion() {
+        return objetivoMeditacionRepository.findAll();
+    }
+
+    @Override
+    public ObjetivoMeditacion getObjetivosMeditacionById (Integer id) {
+        return objetivoMeditacionRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public ObjetivoMeditacion saveObjetivoMeditacion (ObjetivoMeditacion objetivoMeditacion) throws RuntimeException {
+        return objetivoMeditacionRepository.save(objetivoMeditacion);
+    }
+
+    @Override
+    public void deleteObjetivoMeditacion (Integer id) {
+        objetivoMeditacionRepository.deleteById(id);
+    }
+
+    @Override
+    public ObjetivoMeditacion updateObjetivoMeditacion (Integer id, ObjetivoMeditacion objetivoMeditacion) {
+
+        ObjetivoMeditacion ObjetivoMeditacionExistente = objetivoMeditacionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("El objetivo meditacion no se ha encontrado con id: " + id));
+
+        ObjetivoMeditacionExistente.setTiempo_objetivo(objetivoMeditacion.getTiempo_objetivo());
+        ObjetivoMeditacionExistente.setDias_objetivo(objetivoMeditacion.getDias_objetivo());
+        ObjetivoMeditacionExistente.setFk_id_usuario(objetivoMeditacion.getFk_id_usuario());
+
+        return objetivoMeditacionRepository.save(ObjetivoMeditacionExistente);
+    }
+
 }
