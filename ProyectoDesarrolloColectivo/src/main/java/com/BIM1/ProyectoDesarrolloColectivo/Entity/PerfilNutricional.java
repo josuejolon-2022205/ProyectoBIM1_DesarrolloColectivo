ae8aacf540d6e166081b45becb632d3e30140931
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 18:37:13 -0600

    feat: se ha agregado los getter y setter de PerfilNutriciona

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/PerfilNutricional.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/PerfilNutricional.java
@@ -1,5 +1,12 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Perfil_nutricional")
 public class PerfilNutricional {
 
     @Id
@@ -34,4 +41,68 @@ public class PerfilNutricional {
     @Column(name = "fk_id_usuario")
     @NotNull
     private Integer fk_id_usuario;
+
+    public Integer getId_perfil_nutricional() {
+        return id_perfil_nutricional;
+    }
+
+    public void setId_perfil_nutricional(Integer id_perfil_nutricional) {
+        this.id_perfil_nutricional = id_perfil_nutricional;
+    }
+
+    public String getPeso_kg() {
+        return peso_kg;
+    }
+
+    public void setPeso_kg(String peso_kg) {
+        this.peso_kg = peso_kg;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getNivel_actividad() {
+        return nivel_actividad;
+    }
+
+    public void setNivel_actividad(String nivel_actividad) {
+        this.nivel_actividad = nivel_actividad;
+    }
+
+    public String getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(String objetivo) {
+        this.objetivo = objetivo;
+    }
+
+    public Integer getFk_id_usuario() {
+        return fk_id_usuario;
+    }
+
+    public void setFk_id_usuario(Integer fk_id_usuario) {
+        this.fk_id_usuario = fk_id_usuario;
+    }
 }
