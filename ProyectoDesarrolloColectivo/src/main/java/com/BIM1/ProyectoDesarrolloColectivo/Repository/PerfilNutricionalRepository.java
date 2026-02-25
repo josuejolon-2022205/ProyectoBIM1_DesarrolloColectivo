39c6c2453c00d777913c59e158d95af4ff0e8606
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 18:54:37 -0600

    feat: se ha implementado el JpaRepository para PerfilNutricional

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/PerfilNutricionalRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/PerfilNutricionalRepository.java
@@ -1,4 +1,10 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Repository;
 
-public interface PerfilNutricionalRepository {
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.PerfilNutricional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PerfilNutricionalRepository extends JpaRepository<PerfilNutricional, Integer> {
+
 }
