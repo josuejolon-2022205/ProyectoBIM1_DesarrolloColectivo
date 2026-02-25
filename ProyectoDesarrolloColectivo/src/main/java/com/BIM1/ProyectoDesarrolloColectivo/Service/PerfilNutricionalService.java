d64f789ab7edb0265b18fab6bbc13faba121d8a0
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 19:10:11 -0600

    feat: se ha definido la interfaz de servicio para PerfilNutricional

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/PerfilNutricionalService.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/PerfilNutricionalService.java
@@ -1,4 +1,14 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Service;
 
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.PerfilNutricional;
+
+import java.util.List;
+
 public interface PerfilNutricionalService {
+    List<PerfilNutricional> getAListPerfilNuticional();
+    PerfilNutricional getPerfilNutricionalById(Integer id);
+    PerfilNutricional savePerfilNutrcional(PerfilNutricional perfilNutricional) throws RuntimeException;
+    PerfilNutricional updatePerfilNutricional(PerfilNutricional perfilNutricional, Integer id);
+    void deletePerfilNutricional(Integer id);
+
 }
