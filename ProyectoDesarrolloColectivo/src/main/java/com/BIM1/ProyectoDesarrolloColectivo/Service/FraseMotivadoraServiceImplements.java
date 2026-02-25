7be0603c4f40a7f70603a34fe033369c761b952f
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-24 19:47:03 -0600

    feat: se implementaron los metodos de FraseMotivadoraService

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/FraseMotivadoraServiceImplements.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/FraseMotivadoraServiceImplements.java
@@ -1,4 +1,41 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Service;
 
-public class FraseMotivadoraServiceImplements {
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.FraseMotivadora;
+import com.BIM1.ProyectoDesarrolloColectivo.Repository.FraseMotivadoraRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FraseMotivadoraServiceImplements implements FraseMotivadoraService{
+    private final FraseMotivadoraRepository fraseMotivadoraRepository;
+
+    public FraseMotivadoraServiceImplements(FraseMotivadoraRepository fraseMotivadoraRepository) {
+        this.fraseMotivadoraRepository = fraseMotivadoraRepository;
+    }
+
+    @Override
+    public List<FraseMotivadora> getAllFraseMotivadora() {
+        return fraseMotivadoraRepository.findAll();
+    }
+
+    @Override
+    public FraseMotivadora getById(Integer id) {
+        return fraseMotivadoraRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public FraseMotivadora saveFraseMotivadora(FraseMotivadora fraseMotivadora) throws RuntimeException {
+        return fraseMotivadoraRepository.save(fraseMotivadora);
+    }
+
+    @Override
+    public FraseMotivadora updateFraseMotivadora(Integer id, FraseMotivadora fraseMotivadora) {
+        return fraseMotivadoraRepository.save(fraseMotivadora);
+    }
+
+    @Override
+    public void deleteFraseMotivadora(Integer id) {
+        fraseMotivadoraRepository.deleteById(id);
+    }
 }
