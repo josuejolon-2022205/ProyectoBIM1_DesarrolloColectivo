c009d45aaddfce47c3207c1e60a0db447ba2a152
ft-clopez-2021325 <clopez-2021325@kinal.edu.gt>
2026-02-25 09:09:24 -0600

    feat Se han agregado los atributos de EntradaDiario en Entity

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/EntradaDiario.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/EntradaDiario.java
@@ -1,4 +1,30 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
 public class EntradaDiario {
-}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_entrada_diario")
+    private Integer id_entrada_diario;
+
+    @Column(name = "fecha")
+    @NotNull
+    private LocalDate fecha;
+
+    @Column(name = "que_paso")
+    @NotBlank
+    private String que_paso;
+
+    @Column(name = "plan_mañana")
+    private String plan_mañana;
+
+    @Column(name = "reflexion")
+    private String reflexion;
+
+    @Column(name = "fk_id_Usuario")
+    @NotNull
+    private Integer fk_id_usuario;
+
+    public Integer getId_entrada_diario() {
+        return id_entrada_diario;
+    }
