6c4e2364cfa3d33c485cde10a8136397b2ef6aa6
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-24 21:06:04 -0600

    refactor:Se realizaron cambios

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/FraseMotivadoraController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/FraseMotivadoraController.java
@@ -34,7 +34,7 @@ public class FraseMotivadoraController {
     }
 
     @PutMapping
-    public ResponseEntity<Object> updateFraseMotivadora(@PathVariable Integer id, @Valid @RequestBody FraseMotivadora fraseMotivadora){
+    public ResponseEntity<Object> updateFraseMotivadora( Integer id, @Valid @RequestBody FraseMotivadora fraseMotivadora){
         try {
             FraseMotivadora updateFrase = fraseMotivadoraService.updateFraseMotivadora(id,fraseMotivadora);
             return new ResponseEntity<>(updateFrase,HttpStatus.OK);
