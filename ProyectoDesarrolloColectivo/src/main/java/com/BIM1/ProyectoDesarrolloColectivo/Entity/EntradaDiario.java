073cceda3c81d4df6c1c9cc9848a7d0e3eda5f06
ft-clopez-2021325 <clopez-2021325@kinal.edu.gt>
2026-02-25 09:10:39 -0600

    feat se subieron los getter and setter de EntradaDiario en la carpeta Entity

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/EntradaDiario.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/EntradaDiario.java
@@ -28,3 +28,48 @@ public class EntradaDiario {
     public Integer getId_entrada_diario() {
         return id_entrada_diario;
     }
+
+    public void setId_entrada_diario(Integer id_entrada_diario) {
+        this.id_entrada_diario = id_entrada_diario;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getQue_paso() {
+        return que_paso;
+    }
+
+    public void setQue_paso(String que_paso) {
+        this.que_paso = que_paso;
+    }
+
+    public String getPlan_mañana() {
+        return plan_mañana;
+    }
+
+    public void setPlan_mañana(String plan_mañana) {
+        this.plan_mañana = plan_mañana;
+    }
+
+    public String getReflexion() {
+        return reflexion;
+    }
+
+    public void setReflexion(String reflexion) {
+        this.reflexion = reflexion;
+    }
+
+    public Integer getFk_id_usuario() {
+        return fk_id_usuario;
+    }
+
+    public void setFk_id_usuario(Integer fk_id_usuario) {
+        this.fk_id_usuario = fk_id_usuario;
+    }
+}
