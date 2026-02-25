b74001d877c13ff30650ac43d01ca74eb98e517b
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 18:24:28 -0600

    feat: se ha agregado los getter y setter de Libro

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Libro.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Libro.java
@@ -1,5 +1,12 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Libro")
 public class Libro {
 
     @Id
@@ -31,5 +38,59 @@ public class Libro {
     @NotNull
     private int fk_id_usuario;
 
+    public Integer getId_libro() {
+        return id_libro;
+    }
 
+    public void setId_libro(Integer id_libro) {
+        this.id_libro = id_libro;
+    }
+
+    public String getTitulo_libro() {
+        return titulo_libro;
+    }
+
+    public void setTitulo_libro(String titulo_libro) {
+        this.titulo_libro = titulo_libro;
+    }
+
+    public String getAutor_libro() {
+        return autor_libro;
+    }
+
+    public void setAutor_libro(String autor_libro) {
+        this.autor_libro = autor_libro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getCantidad_pag() {
+        return cantidad_pag;
+    }
+
+    public void setCantidad_pag(int cantidad_pag) {
+        this.cantidad_pag = cantidad_pag;
+    }
+
+    public int getCantidad_leido() {
+        return cantidad_leido;
+    }
+
+    public void setCantidad_leido(int cantidad_leido) {
+        this.cantidad_leido = cantidad_leido;
+    }
+
+    public int getFk_id_usuario() {
+        return fk_id_usuario;
+    }
+
+    public void setFk_id_usuario(int fk_id_usuario) {
+        this.fk_id_usuario = fk_id_usuario;
+    }
 }
