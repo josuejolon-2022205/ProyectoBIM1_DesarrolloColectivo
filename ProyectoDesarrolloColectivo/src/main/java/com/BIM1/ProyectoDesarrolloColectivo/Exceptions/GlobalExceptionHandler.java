0accbbe9a5d7831510ff5075ba8ad19ad7e9ef91
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 20:51:43 -0600

    feat: se agrego la excepcion para los ids que no existen

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Exceptions/GlobalExceptionHandler.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> validarMapeo(Exception e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Error", "no se encontro el id"));
+
+    }
+
 }
