d443eed3143162bf23cfce8ef20e169d77566146
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-25 10:06:08 -0600

    feat: se implementaron los metodos de ObjetivosServive

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ObjetivosServiceImplements.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ObjetivosServiceImplements.java
@@ -1,4 +1,41 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Service;
 
-public class ObjetivosServiceImplements {
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.Objetivos;
+import com.BIM1.ProyectoDesarrolloColectivo.Repository.ObjetivosRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ObjetivosServiceImplements implements ObjetivosService{
+    public final ObjetivosRepository objetivosRepository;
+
+    public ObjetivosServiceImplements(ObjetivosRepository objetivosRepository) {
+        this.objetivosRepository = objetivosRepository;
+    }
+
+    @Override
+    public List<Objetivos> getAllObjetivos() {
+        return objetivosRepository.findAll() ;
+    }
+
+    @Override
+    public Objetivos getById(Integer id) {
+        return objetivosRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Objetivos saveObjetivos(Objetivos objetivos) throws RuntimeException {
+        return objetivosRepository.save(objetivos);
+    }
+
+    @Override
+    public Objetivos updateObjetivos(Integer id, Objetivos objetivos) {
+        return objetivosRepository.save(objetivos);
+    }
+
+    @Override
+    public void deleteObjetivos(Integer id) {
+        objetivosRepository.deleteById(id);
+    }
 }
