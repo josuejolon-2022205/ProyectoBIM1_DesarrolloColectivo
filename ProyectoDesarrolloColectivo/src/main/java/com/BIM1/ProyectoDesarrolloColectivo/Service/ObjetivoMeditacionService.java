fc969dad7e0a93c22d1b9668e4f7e7f341b355a5
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-02-24 21:57:35 -0600

    feat: Se ha agregado el ObjetivoMeditacion en el Service

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ObjetivoMeditacionService.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ObjetivoMeditacionService.java
@@ -1,4 +1,16 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Service;
 
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.ObjetivoMeditacion;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface ObjetivoMeditacionService {
+
+    List<ObjetivoMeditacion> getAllOjetivosMeditacion();
+    ObjetivoMeditacion getObjetivosMeditacionById (Integer id);
+    ObjetivoMeditacion saveObjetivoMeditacion (ObjetivoMeditacion objetivoMeditacion) throws RuntimeException;
+    ObjetivoMeditacion updateObjetivoMeditacion (Integer id, ObjetivoMeditacion objetivoMeditacion);
+    void deleteObjetivoMeditacion (Integer id);
 }
