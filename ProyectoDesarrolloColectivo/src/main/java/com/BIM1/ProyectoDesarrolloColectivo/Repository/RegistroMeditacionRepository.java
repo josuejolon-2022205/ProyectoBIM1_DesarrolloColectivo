a5cef1257b6d1ef9a961559d4e4a8596efd012ba
ft-clopez-2021325 <clopez-2021325@kinal.edu.gt>
2026-02-25 10:54:41 -0600

    feat: Se cargo RegistroMeditainRepository completado

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/RegistroMeditacionRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/RegistroMeditacionRepository.java
@@ -1,4 +1,10 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Repository;
 
-public interface RegistroMeditacionRepository {
+
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.RegistroMeditacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistroMeditacionRepository extends JpaRepository<RegistroMeditacion, Integer> {
 }
