f7f72e10ba7f9044a1154f7aa8ba09621f9db267
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-24 18:54:22 -0600

    feat: se ha implementado el JpaRepository para Libro

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/LibroRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/LibroRepository.java
@@ -1,4 +1,10 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Repository;
 
-public interface LibroRepository {
+
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Integer> {
 }
