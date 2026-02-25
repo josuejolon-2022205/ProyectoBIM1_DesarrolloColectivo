a0993076e28c5b3b535ee310bb43ba6fad934f23
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-02-25 10:08:12 -0600

    feat: se implementaron los endpoints POST en RachaEjercicioController para crear rachas

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/RachaEjercicioController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/RachaEjercicioController.java
@@ -49,6 +49,36 @@ public class RachaEjercicioController {
     }
 
 
+@PostMapping("/agregar")
+public ResponseEntity<Object> agregarRacha(@RequestBody RachaEjercicio racha) {
+    try {
+        RachaEjercicio nueva = rachaEjercicioService.saveRacha(racha);
+        return new ResponseEntity<>(nueva, HttpStatus.CREATED);
+    } catch (IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Error al crear la racha: " + e.getMessage());
+    }
+}
+
+
+@PostMapping("/usuario/{idUsuario}/fecha/{fecha}")
+public ResponseEntity<Object> crearRachaPorUsuario(
+        @PathVariable Integer idUsuario,
+        @PathVariable LocalDate fecha) {
+
+    try {
+        RachaEjercicio nueva = rachaEjercicioService.addRacha(idUsuario, fecha);
+        return new ResponseEntity<>(nueva, HttpStatus.CREATED);
+    } catch (IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Error al crear la racha por usuario y fecha: " + e.getMessage());
+    }
+}
+
 }
 
 
