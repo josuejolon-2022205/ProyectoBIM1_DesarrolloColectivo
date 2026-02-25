610350a457ff285067841936db643aeb8e2172b4
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-24 19:13:30 -0600

    feat:Se agrego metodo para validacion de duplicados

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/FraseMotivadoraRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/FraseMotivadoraRepository.java
@@ -1,4 +1,13 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Repository;
 
-public interface FraseMotivadoraRepository {
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.FraseMotivadora;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FraseMotivadoraRepository extends JpaRepository<FraseMotivadora,Integer> {
+    boolean existsByTextoAndAutor(
+            String texto,
+            String autor
+    );
 }
