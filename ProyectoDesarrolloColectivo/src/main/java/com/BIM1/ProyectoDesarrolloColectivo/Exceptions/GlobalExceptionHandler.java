7a999f1534be5b1b596e373c1f92439528d88aeb
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-25 17:07:35 -0600

    feat: se agrego la importacion del ConstraitViolationException

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Exceptions/GlobalExceptionHandler.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Exceptions;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
