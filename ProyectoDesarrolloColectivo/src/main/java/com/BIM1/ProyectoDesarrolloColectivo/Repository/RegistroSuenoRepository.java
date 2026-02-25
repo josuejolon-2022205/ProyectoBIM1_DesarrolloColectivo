e39eeaf5ade31914a0385eaef058750f2c9e1680
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-02-24 21:19:55 -0600

    feat: Se ha agregado el JpaRepository en el RegistroSueñoRepository

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/RegistroSuenoRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/RegistroSuenoRepository.java
@@ -1,4 +1,10 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Repository;
 
-public interface RegistroSueñoRepository {
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.RegistroSueno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistroSuenoRepository extends JpaRepository<RegistroSueno, Integer> {
+
 }
