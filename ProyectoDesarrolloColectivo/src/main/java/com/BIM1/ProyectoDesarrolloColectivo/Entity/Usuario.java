7dd54dfd64954431a62803526d4a610b064edcb6
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-02-24 20:16:34 -0600

    feat: Se han agregado los métodos para el Usuario en Entity

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Usuario.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Usuario.java
@@ -1,5 +1,62 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Integer id_usuario;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Column(name = "nombre_completo")
+    private String nombre_completo;
+
+    @NotBlank(message = "El correo no puede estar vacío")
+    @Column(name = "correo_usuario")
+    private String correo_usuario;
+
+    @NotBlank(message = "La contraseña no puede estar vacío")
+    @Column(name = "contraseña")
+    private String contraseña;
+
+    public Usuario() {
+
+    }
+
+    public Integer getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(Integer id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre_completo() {
+        return nombre_completo;
+    }
+
+    public void setNombre_completo(String nombre_completo) {
+        this.nombre_completo = nombre_completo;
+    }
+
+    public String getCorreo_usuario() {
+        return correo_usuario;
+    }
+
+    public void setCorreo_usuario(String correo_usuario) {
+        this.correo_usuario = correo_usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 }
