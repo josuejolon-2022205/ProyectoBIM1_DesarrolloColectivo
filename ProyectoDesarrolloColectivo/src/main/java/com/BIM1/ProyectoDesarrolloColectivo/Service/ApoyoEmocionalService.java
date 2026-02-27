e60a35f302d9d9fc43cca432e54944384482be03
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-26 20:20:42 -0600

    refactor:Se hicieron cambios en los metodos

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ApoyoEmocionalService.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ApoyoEmocionalService.java
@@ -1,13 +1,13 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Service;
 
-import com.BIM1.ProyectoDesarrolloColectivo.Entity.FraseMotivadora;
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.ApoyoEmocional;
 
 import java.util.List;
 
 public interface ApoyoEmocionalService {
-    List<FraseMotivadora> getAllFraseMotivadora();
-    FraseMotivadora getById(Integer id);
-    FraseMotivadora saveFraseMotivadora(FraseMotivadora fraseMotivadora) throws RuntimeException;
-    FraseMotivadora updateFraseMotivadora(Integer id,FraseMotivadora fraseMotivadora);
+    List<ApoyoEmocional> getAllFraseMotivadora();
+    ApoyoEmocional getById(Integer id);
+    ApoyoEmocional saveFraseMotivadora(ApoyoEmocional apoyoEmocional) throws RuntimeException;
+    ApoyoEmocional updateFraseMotivadora(Integer id,ApoyoEmocional apoyoEmocional);
     void deleteFraseMotivadora(Integer id);
 }
