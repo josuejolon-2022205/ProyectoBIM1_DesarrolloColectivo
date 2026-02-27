b03f1ef9ff5b2bbb897507d0a1591535ee7354d7
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-26 20:25:06 -0600

    feat: se implementaron los métodos de ApoyoEmocionalService

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ApoyoEmocionalServiceImplements.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ApoyoEmocionalServiceImplements.java
@@ -1,4 +1,42 @@
 package com.BIM1.ProyectoDesarrolloColectivo.Service;
 
-public class ApoyoEmocionalServiceImplements {
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.ApoyoEmocional;
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.FraseMotivadora;
+import com.BIM1.ProyectoDesarrolloColectivo.Repository.ApoyoEmocionalRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ApoyoEmocionalServiceImplements implements ApoyoEmocionalService{
+    private final ApoyoEmocionalRepository apoyoEmocionalRepository;
+
+    public ApoyoEmocionalServiceImplements(ApoyoEmocionalRepository apoyoEmocionalRepository) {
+        this.apoyoEmocionalRepository = apoyoEmocionalRepository;
+    }
+
+    @Override
+    public List<ApoyoEmocional> getAllFraseMotivadora() {
+        return apoyoEmocionalRepository.findAll();
+    }
+
+    @Override
+    public ApoyoEmocional getById(Integer id) {
+        return apoyoEmocionalRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public ApoyoEmocional saveFraseMotivadora(ApoyoEmocional apoyoEmocional) throws RuntimeException {
+        return apoyoEmocionalRepository.save(apoyoEmocional);
+    }
+
+    @Override
+    public ApoyoEmocional updateFraseMotivadora(Integer id, ApoyoEmocional apoyoEmocional) {
+        return apoyoEmocionalRepository.save(apoyoEmocional);
+    }
+
+    @Override
+    public void deleteFraseMotivadora(Integer id) {
+        apoyoEmocionalRepository.deleteById(id);
+    }
 }
