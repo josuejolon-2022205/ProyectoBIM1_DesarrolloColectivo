c0a907dfce6bebe677ec59af16d3f5076e19a172
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-27 19:30:29 -0600

    refactor: Se cambio el nombre de la clase

---
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Exceptions/CustomException.java
@@ -0,0 +1,7 @@
+package com.BIM1.ProyectoDesarrolloColectivo.Exceptions;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
