366da826a47772397d01639734727231b1373c9d
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-02-28 15:20:24 -0600

    refactor: se realizaron cambios en el controller de RachaEjercio y se simplifico la ruta

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/RachaEjercicioController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/RachaEjercicioController.java
@@ -24,13 +24,13 @@ public class RachaEjercicioController {
         return rachaEjercicioService.getRachasByUsuario(idUsuario);
     }
 
-
-    @GetMapping("/usuario/{idUsuario}/entre")
-    public List<RachaEjercicio> getByUsuarioYRango(@PathVariable Integer idUsuario, @RequestParam String inicio, @RequestParam String fin) {
+    @GetMapping("/usuario/{idUsuario}/entre/{inicio}/{fin}")
+    public List<RachaEjercicio> getByUsuarioYRango( @PathVariable Integer idUsuario, @PathVariable String inicio, @PathVariable String fin) {
         LocalDate ini = LocalDate.parse(inicio);
         LocalDate fn = LocalDate.parse(fin);
         return rachaEjercicioService.getRachasByUsuarioAndRango(idUsuario, ini, fn);
     }
+
 
 
 
@@ -48,16 +48,12 @@ public class RachaEjercicioController {
 
 
 
-    @PostMapping("/usuario/{idUsuario}")
-    public ResponseEntity<Object> crearRachaPorUsuario( @PathVariable Integer idUsuario, @RequestParam String fecha) {
-        try {
-            LocalDate fechaParseada = LocalDate.parse(fecha);
-            RachaEjercicio nueva = rachaEjercicioService.addRacha(idUsuario, fechaParseada);
+    @PostMapping("/usuario/{idUsuario}/fecha/{fecha}")
+    public ResponseEntity<Object> crearRachaPorUsuario( @PathVariable Integer idUsuario, @PathVariable String fecha) {
+        try { LocalDate f = LocalDate.parse(fecha);
+            RachaEjercicio nueva = rachaEjercicioService.addRacha(idUsuario, f);
             return new ResponseEntity<>(nueva, HttpStatus.CREATED);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) { return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
     }
