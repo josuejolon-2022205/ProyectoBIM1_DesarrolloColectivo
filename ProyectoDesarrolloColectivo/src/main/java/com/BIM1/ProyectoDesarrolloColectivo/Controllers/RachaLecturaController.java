b885fe66d12b0b38d80c4c71026623154ff89c57
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-02-28 17:46:38 -0600

    feat: se implemento el manejo de exceptions para rachas

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/RachaLecturaController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/RachaLecturaController.java
@@ -55,9 +55,16 @@ public class RachaLecturaController {
 
     @PostMapping("/usuario/{idUsuario}/fecha/{fecha}")
     public ResponseEntity<Object> crearRachaPorUsuario( @PathVariable Integer idUsuario, @PathVariable String fecha) {
-        try {LocalDate fechaParseada;
-            try { fechaParseada = LocalDate.parse(fecha);
-            } catch (Exception e) { return ResponseEntity.badRequest().body("Formato de fecha inválido. Usa YYYY-MM-DD.");
+        try {
+            LocalDate fechaParseada;
+            try {
+                fechaParseada = LocalDate.parse(fecha);
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body("Formato de fecha inválido. Usa YYYY-MM-DD.");
+            }
+            LocalDate hoy = LocalDate.now();
+            if (fechaParseada.isBefore(hoy)) {
+                return ResponseEntity.badRequest().body("La fecha no puede ser pasada");
             }
             RachaLectura nueva = rachaLecturaService.addRacha(idUsuario, fechaParseada);
             return new ResponseEntity<>(nueva, HttpStatus.CREATED);
