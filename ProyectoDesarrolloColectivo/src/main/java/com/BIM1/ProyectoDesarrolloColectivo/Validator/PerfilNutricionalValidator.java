1906aa4d81b9333bb0dded2d2285f523985fd93c
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-27 18:22:03 -0600

    refactor: se elimino el codigo redundante que fue cubierto por anotaciones en la entity de PerfilNutricional

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Validator/PerfilNutricionalValidator.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Validator/PerfilNutricionalValidator.java
@@ -17,27 +17,6 @@ public class PerfilNutricionalValidator {
 
     public void PerfilNutricionalValidaciones(PerfilNutricional perfil) {
 
-        if (perfil.getPeso_kg() <= 0) {
-            throw new Exception("El peso debe ser un valor positivo mayor a 0.");
-        }
-        if (perfil.getPeso_kg() > 500) {
-            throw new Exception("El peso ingresado no es válido (máximo 500 kg).");
-        }
-
-        if (perfil.getAltura() <= 0.0) {
-            throw new Exception("La altura debe ser un valor positivo mayor a 0.");
-        }
-        if (perfil.getAltura() < 0.50 || perfil.getAltura() > 2.50) {
-            throw new Exception("La altura debe estar entre 0.50 y 2.50 metros.");
-        }
-
-        if (perfil.getEdad() <= 0) {
-            throw new Exception("La edad debe ser un valor positivo mayor a 0.");
-        }
-        if (perfil.getEdad() < 5 || perfil.getEdad() > 120) {
-            throw new Exception("La edad debe estar entre 5 y 120 años.");
-        }
-
         List<String> generosValidos = Arrays.asList("Femenino", "Masculino");
         if (!generosValidos.contains(perfil.getGenero())) {
             throw new Exception("El género debe ser 'Masculino' o 'Femenino'.");
@@ -48,12 +27,5 @@ public class PerfilNutricionalValidator {
             throw new Exception("El nivel de actividad debe ser 'bajo', 'medio' o 'alto'.");
         }
 
-        if (perfil.getObjetivo().trim().length() > 75) {
-            throw new Exception("El objetivo no puede superar los 75 caracteres.");
-        }
-
-        if (perfil.getFk_id_usuario() <= 0) {
-            throw new Exception("El ID del usuario debe ser un número positivo válido.");
-        }
     }
 }
