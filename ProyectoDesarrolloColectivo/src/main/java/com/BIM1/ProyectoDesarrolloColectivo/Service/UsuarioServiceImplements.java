cf913cd9edec71cf15728ccafc1f8d3799e92202
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-03-02 02:16:40 -0600

    feat: Se ha agregado el método para poder mandar los mensajes de bienvenida en el UsuarioServiceImplements

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/UsuarioServiceImplements.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/UsuarioServiceImplements.java
@@ -7,6 +7,9 @@ import com.BIM1.ProyectoDesarrolloColectivo.Validator.UsuarioValidator;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 
 @Service
 public class UsuarioServiceImplements implements UsuarioService {
@@ -14,7 +17,7 @@ public class UsuarioServiceImplements implements UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioValidator usuarioValidator;
 
-    public UsuarioServiceImplements (UsuarioRepository usuarioRepository, UsuarioValidator usuarioValidator) {
+    public UsuarioServiceImplements(UsuarioRepository usuarioRepository, UsuarioValidator usuarioValidator) {
         this.usuarioRepository = usuarioRepository;
         this.usuarioValidator = usuarioValidator;
     }
@@ -25,33 +28,50 @@ public class UsuarioServiceImplements implements UsuarioService {
     }
 
     @Override
-    public Usuario getUsuariosById (Integer id) {
+    public Usuario getUsuariosById(Integer id) {
         return usuarioRepository.findById(id).orElse(null);
     }
 
     @Override
-    public Usuario saveUsuario (Usuario usuario) throws RuntimeException {
+    public Usuario saveUsuario(Usuario usuario) {
+
+        String correo = usuario.getCorreoUsuario().trim().toLowerCase();
+        String contraseña = usuario.getContraseña();
+
+        Usuario usuarioExistente = usuarioRepository.findByCorreoUsuario(correo);
+
+        if (usuarioExistente != null) {
+
+            if (usuarioExistente.getContraseña().equals(contraseña)) {
+                throw new Exception("Bienvenido de nuevo " + usuarioExistente.getNombre_completo());
+            }
+            throw new IllegalArgumentException("El correo ya está registrado pero la contraseña es incorrecta intente de nuevo por favor"
+            );
+        }
+
         usuarioValidator.UsuarioValidar(usuario);
-        return usuarioRepository.save(usuario);
+
+        Usuario nuevoUsuario = usuarioRepository.save(usuario);
+
+        throw new Exception("Bienvenido " + nuevoUsuario.getNombre_completo() + ", tu cuenta fue creada correctamente");
     }
 
     @Override
-    public void deleteUsuario (Integer id) {
+    public void deleteUsuario(Integer id) {
         usuarioRepository.deleteById(id);
     }
 
     @Override
-    public Usuario updateUsuario (Integer id, Usuario usuario) {
+    public Usuario updateUsuario(Integer id, Usuario usuario) {
 
         Usuario usuarioExistente = usuarioRepository.findById(id)
                 .orElseThrow(() -> new Exception("El usuario no se ha encontrado con id: " + id));
 
         usuarioValidator.UsuarioValidar(usuario);
         usuarioExistente.setNombre_completo(usuario.getNombre_completo());
-        usuarioExistente.setCorreo_usuario(usuario.getCorreo_usuario());
+        usuarioExistente.setCorreoUsuario(usuario.getCorreoUsuario());
         usuarioExistente.setContraseña(usuario.getContraseña());
 
         return usuarioRepository.save(usuarioExistente);
     }
-
 }
