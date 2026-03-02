99cebf1ae9755116c0183eb467009fffaf8cc2a4
Cristopher <clopez-2021325@kinal.edu.gt>
2026-03-02 14:57:50 -0600

    feat:Se crearon las exceptiones

---
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Exceptions/Excepciones.java
@@ -0,0 +1,7 @@
+package com.BIM1.ProyectoDesarrolloColectivo.Exceptions;
+
+public class Excepciones extends RuntimeException {
+    public Excepciones(String message) {
+        super(message);
+    }
+}
