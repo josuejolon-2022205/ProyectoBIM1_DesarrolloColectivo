697a1b8c4f1c2db5c8943145e75b9f5194270c91
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-03-02 02:07:43 -0600

    feat: Se ha agregado un buscarPorCorreoUsuario en el UsuarioRepository

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/UsuarioRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/UsuarioRepository.java
@@ -2,10 +2,8 @@ package com.BIM1.ProyectoDesarrolloColectivo.Repository;
 
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
+    Usuario findByCorreoUsuario(String correoUsuario);
 }
-
