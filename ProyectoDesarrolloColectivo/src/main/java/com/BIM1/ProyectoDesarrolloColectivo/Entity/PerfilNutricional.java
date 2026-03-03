8fca1dc49f171c444745ec036ad52841eded4aaf
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-03-03 16:52:43 -0600

    feat: se agrego la anotacion @Min para validar un minimo de peso en la entity de PerfilNutricional

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/PerfilNutricional.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/PerfilNutricional.java
@@ -16,7 +16,8 @@ public class PerfilNutricional {
     @Column(name = "peso_kg")
     @NotNull(message = "el peso no puede estar vacio o nulo")
     @Positive(message = "el peso debe de ser mayor a 0")
-    @Max(value = 300, message = "el peso no puede ser mayor a 300")
+    @Max(value = 300, message = "el peso no puede ser mayor a 300 kg")
+    @Min(value = 45, message = "el peso no puede ser menor a 45 kg")
     private int peso_kg;
 
     @Column(name = "altura")
