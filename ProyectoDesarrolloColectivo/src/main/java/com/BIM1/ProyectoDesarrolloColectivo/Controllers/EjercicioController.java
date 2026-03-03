7ae02c55436380b6a267500d117fea547e8b3f3f
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-03-03 16:50:41 -0600

    fix: se quito el try catch de Ejercicio porque el manejo de las excepciones ya que el GlobalExceptionHandler ya maneja estas mismas

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/EjercicioController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/EjercicioController.java
@@ -4,7 +4,6 @@ package com.BIM1.ProyectoDesarrolloColectivo.Controllers;
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.Ejercicio;
 import com.BIM1.ProyectoDesarrolloColectivo.Service.EjercicioService;
 import jakarta.validation.Valid;
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,45 +26,25 @@ public class EjercicioController {
 
     @PostMapping
     public ResponseEntity<Object> createEjercicio(@Valid @RequestBody Ejercicio ejercicio){
-        try {
             Ejercicio ejercicio1 = ejercicioService.saveEjercicio(ejercicio);
             return new ResponseEntity<>(ejercicio1, HttpStatus.CREATED);
-        }catch (IllegalArgumentException e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateEjercicio(@PathVariable Integer id, @Valid @RequestBody Ejercicio ejercicio) {
-        try {
             Ejercicio ejercicio1 = ejercicioService.updateEjercicio(id, ejercicio);
             return new ResponseEntity<>(ejercicio1, HttpStatus.OK);
-        }catch (IllegalArgumentException e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-
-        }
-
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteEjercicio(@PathVariable Integer id){
-        try {
             ejercicioService.deleteEjercicio(id);
             return ResponseEntity.noContent().build();
-        }catch (ObjectNotFoundException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-
-        }
-
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getEjercicioById(@PathVariable Integer id){
-        try{
             Ejercicio ejercicio = ejercicioService.getEjercicioById(id);
-            return ResponseEntity.ok(ejercicio);
-        }catch (ObjectNotFoundException e){
-            return ResponseEntity.notFound().build();
-        }
+             return ResponseEntity.ok(ejercicio);
     }
 }
