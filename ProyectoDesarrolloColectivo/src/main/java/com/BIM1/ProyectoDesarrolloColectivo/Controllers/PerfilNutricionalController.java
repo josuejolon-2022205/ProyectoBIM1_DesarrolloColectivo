2fae9fbbfc7182bf9b43daa3fddfaeb2fd0fddd7
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-03-03 16:51:09 -0600

    fix: se quito el try catch de PerfilNutricionalController porque el manejo de las excepciones ya que el GlobalExceptionHandler ya maneja estas mismas

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/PerfilNutricionalController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/PerfilNutricionalController.java
@@ -27,42 +27,25 @@ public class PerfilNutricionalController {
 
     @PostMapping
     public ResponseEntity<Object> savePerfilNutricional(@Valid @RequestBody PerfilNutricional perfilNutricional){
-        try {
             PerfilNutricional perfilNutricional1 = perfilNutricionalService.savePerfilNutrcional(perfilNutricional);
             return new ResponseEntity<>(perfilNutricional1, HttpStatus.CREATED);
-        }catch (IllegalArgumentException e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Object> updatePerfilNutricional(@PathVariable Integer id, @Valid @RequestBody PerfilNutricional perfilNutricional){
-        try {
             PerfilNutricional perfilNutricional1 = perfilNutricionalService.updatePerfilNutricional(id, perfilNutricional);
             return new ResponseEntity<>(perfilNutricional1, HttpStatus.OK);
-        }catch (IllegalArgumentException e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-
-        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deletePerfilNutricional(@PathVariable Integer id){
-        try {
             perfilNutricionalService.deletePerfilNutricional(id);
             return ResponseEntity.noContent().build();
-        }catch (ObjectNotFoundException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getPerfilNutricionalById(@PathVariable Integer id){
-        try {
             PerfilNutricional perfilNutricional = perfilNutricionalService.getPerfilNutricionalById(id);
             return ResponseEntity.ok(perfilNutricional);
-        }catch (ObjectNotFoundException e){
-            return ResponseEntity.notFound().build();
-        }
     }
 }
